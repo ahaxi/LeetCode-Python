9cd25f09006d179bc3fe53616e88bc153360cacb
ahaxi <yixifu23@gmail.com>
2016-09-15 10:24:33 -0700

    Update Reverse_Linked_List.py

--- Reverse_Linked_List.py
+++ Reverse_Linked_List.py
@@ -20,3 +20,22 @@ class Solution:
             prev = current 
             current = next_node
         return prev        
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head == None or head.next == None:
+            return False
+        
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            
+            slow = slow.next
+            fast = fast.next.next
+        return True
